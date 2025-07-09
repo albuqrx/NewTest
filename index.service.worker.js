bf509ad8ea07751cd5533ef75b9a9e3a956cde89
albuqrx <albuqrx@yandex.ru>
2025-07-09 09:40:46 +0300

    fix png

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1752009238|359809320';
+const CACHE_VERSION = '1752043198|47927439';
 /** @type {string} */
 const CACHE_PREFIX = 'SnakeNew-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
